7f918e777995d3ea50e778406b2e62ccc500e813
9bagel <9bagel@gmail.com>
2017-11-01 20:37:22 +0300

    few EZ tasks

--- 1.JavaSyntax/src/com/javarush/task/task05/task0514/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task05/task0514/Solution.java
@@ -7,9 +7,20 @@ package com.javarush.task.task05.task0514;
 public class Solution {
     public static void main(String[] args) {
         //напишите тут ваш код
+        Person person = new Person();
+        person.initialize("JavaRush", 20);
     }
 
     static class Person {
         //напишите тут ваш код
+        int age;
+        String name;
+
+        public void initialize(String name, int age) {
+            this.age = age;
+            this.name = name;
+        }
     }
+
+
 }
---
+++ 2.JavaCore/src/com/javarush/task/task19/task1922/Solution.java
@@ -0,0 +1,40 @@
+package com.javarush.task.task19.task1922;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
+/* 
+Ищем нужные строки
+*/
+
+public class Solution {
+    public static List<String> words = new ArrayList<String>();
+
+    static {
+        words.add("файл");
+        words.add("вид");
+        words.add("В");
+    }
+
+    public static void main(String[] args) {
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+             BufferedReader fileReader = new BufferedReader(new FileReader(reader.readLine()))) {
+            while (fileReader.ready()) {
+                String tmp = fileReader.readLine();
+                int counter = 0;
+                for (String word : words) {
+                    if (tmp.contains(word))
+                        ++counter;
+                }
+                if (counter == 2)
+                    System.out.println(tmp);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
