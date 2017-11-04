539be2db8526458f514745e1754e9fe47204fc02
9bagel <9bagel@gmail.com>
2017-11-04 13:44:54 +0300

    task 1914

--- 2.JavaCore/src/com/javarush/task/task19/task1914/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task19/task1914/Solution.java
@@ -4,10 +4,35 @@ package com.javarush.task.task19.task1914;
 Решаем пример
 */
 
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
 public class Solution {
     public static TestString testString = new TestString();
 
     public static void main(String[] args) {
+        PrintStream consoleOutput = System.out;
+
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+
+        PrintStream printStream = new PrintStream(outputStream);
+
+        System.setOut(printStream);
+
+        testString.printSomething();
+
+        System.setOut(consoleOutput);
+
+        String[] arguments = outputStream.toString().split(" ");
+        int a = Integer.parseInt(arguments[0]);
+        int b = Integer.parseInt(arguments[2]);
+
+        if (arguments[1].equals("+"))
+            System.out.println(String.valueOf(a) + " + " + String.valueOf(b) + " = " + (a + b));
+        else if (arguments[1].equals("*"))
+            System.out.println(String.valueOf(a) + " * " + String.valueOf(b) + " = " + (a * b));
+        else if (arguments[1].equals("-"))
+            System.out.println(String.valueOf(a) + " - " + String.valueOf(b) + " = " + (a - b));
     }
 
     public static class TestString {
