543cf35a58b799edcffa837108bd594fe0adc7b3
9bagel <9bagel@gmail.com>
2017-11-27 20:50:59 +0300

    inner classes

---
+++ 3.JavaMultithreading/src/com/javarush/task/task23/task2304/NameDataProvider.java
@@ -0,0 +1,10 @@
+package com.javarush.task.task23.task2304;
+
+import java.util.Map;
+
+public class NameDataProvider implements Solution.DbDataProvider {
+    @Override
+    public void refreshAllData(Map criteria) {
+
+    }
+}
---
+++ 3.JavaMultithreading/src/com/javarush/task/task23/task2304/TaskDataProvider.java
@@ -0,0 +1,10 @@
+package com.javarush.task.task23.task2304;
+
+import java.util.Map;
+
+public class TaskDataProvider implements Solution.DbDataProvider {
+    @Override
+    public void refreshAllData(Map criteria) {
+
+    }
+}
