2eb56ad824f10079678e5a4eacfa9acb005eda7c
9bagel <9bagel@gmail.com>
2018-01-19 16:12:52 +0300

    Enum

---
+++ 3.JavaMultithreading/src/com/javarush/task/task25/task2502/Solution.java
@@ -0,0 +1,40 @@
+package com.javarush.task.task25.task2502;
+
+import java.util.*;
+
+/* 
+Машину на СТО не повезем!
+*/
+public class Solution {
+    public static enum Wheel {
+        FRONT_LEFT,
+        FRONT_RIGHT,
+        BACK_LEFT,
+        BACK_RIGHT
+    }
+
+    public static class Car {
+        protected List<Wheel> wheels;
+
+        public Car() throws Exception {
+            //init wheels here
+            wheels = new ArrayList<>();
+            int counter = 0;
+            for (String s : loadWheelNamesFromDB()) {
+                wheels.add(Wheel.valueOf(s));
+                counter++;
+            }
+            if (counter != 4)
+                throw new Exception();
+        }
+
+
+        protected String[] loadWheelNamesFromDB() {
+            //this method returns mock data
+            return new String[]{"FRONT_LEFT", "FRONT_RIGHT", "BACK_LEFT", "BACK_RIGHT"};
+        }
+    }
+
+    public static void main(String[] args) {
+    }
+}
